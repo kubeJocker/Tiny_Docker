5b7c79b08d52566d99a14bb40f9f32df7daff018
Lucifer <guobiaohan1998@163.com>
2023-02-18 01:20:40 +0800

    tag0.2

--- run.go
+++ run.go
@@ -1,16 +1,35 @@
 package main
 
 import (
-	"Tiny_Docker/container"
+	"TinyDocker/cgroup"
+	subsystems "TinyDocker/cgroup/subsystem"
+	"TinyDocker/container"
 	log "github.com/sirupsen/logrus"
 	"os"
+	"strings"
 )
 
-func Run(tty bool, command string) {
-	parent := container.NewParentProcess(tty, command)
+func Run(tty bool, comArray []string, res *subsystems.ResourceConfig) {
+	parent := container.NewParentProcess(tty)
+	if parent == nil {
+		log.Errorf("New parent process error")
+		return
+	}
 	if err := parent.Start(); err != nil {
 		log.Error(err)
 	}
+
+	cgroupManager := cgroup.NewCgroupManager("tinydocker-cgroup")
+	defer cgroupManager.Destroy()
+	cgroupManager.Set(res)
+	cgroupManager.Apply(parent.Process.Pid)
+	sendInitCommand(comArray, writePipe)
 	parent.Wait()
-	os.Exit(-1)
+}
+
+func sendInitCommand(comArray []string, writePipe *os.File) {
+	command := strings.Join(comArray, " ")
+	log.Infof("command all is %s", command)
+	writePipe.WriteString(command)
+	writePipe.Close()
 }
